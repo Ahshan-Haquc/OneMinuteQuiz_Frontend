9694462041fd315f119f0d882c26c99a0b2665a6
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-05-14 11:00:51 +0600

    Created context for day-light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { DarkModeProvider } from "./context/DarkModeContext";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
+  </React.StrictMode>
+);
