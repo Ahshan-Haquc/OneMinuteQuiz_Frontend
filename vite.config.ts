d48226fceec6583c5c7cdc3622253ee3773dfba2
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-05-14 10:57:34 +0600

    Added Assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/signup': 'http://localhost:3000',
+      '/login': 'http://localhost:3000',
+      '/me': 'http://localhost:3000',
+    }
+  }
 })
